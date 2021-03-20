f46e746b42e7854a29384b0f5f55b7a725b7032f
Julio Romero <julio_ale21@hotmail.com>
2021-03-20 19:18:42 -0300

    feat(products): added query to get products count

--- data/resolvers.js
+++ data/resolvers.js
@@ -36,7 +36,16 @@ export const resolvers = {
           else resolve(product);
         });
       });
-    }
+    },
+
+    totalProducts: (root) => {
+      return new Promise((resolve, reject) => {
+        Products.countDocuments({}, (error, count) => {
+          if (error) reject(error);
+          else resolve(count);
+        })
+      });
+    },
   },
   Mutation: {
     createCustomer : (root, { input }) => {
