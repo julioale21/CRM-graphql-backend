6447902d408a1f70dfaca0b93adeccbb457d18c8
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-03-30 11:47:25 -0300

    chore: added logic to get token when user do login and verify that it's correct

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./data/schema";
 import { resolvers } from "./data/resolvers";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "variables.env"
+});
 
 const app = express();
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async({ req }) => {
+    const token = req.headers["authorization"];
+
+    if (token !== "null") {
+      try {
+        const currentUser = await jwt.verify(token, process.env.SECRET);
+        req.currentUser = currentUser;
+        return {
+          currentUser,
+        };
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+});
 
 server.applyMiddleware({ app });
 
